b7262fc9aedb8dfdc3c582ad6ea4878f6e3408b9
Bert.Gibbons <Bert.Gibbons@APHCNB101375.amed.ds.army.mil>
2018-04-19 14:06:07 -0400

    Fixed Flamingo

--- FlamingoJavaFX/src/main/java/app/Flamingo.java
+++ FlamingoJavaFX/src/main/java/app/Flamingo.java
@@ -14,13 +14,15 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import netgame.common.Client;
+import pkgCore.Action;
 import pkgCore.GamePlay;
 import pkgCore.Player;
 import pkgCore.Table;
+import pkgEnum.eAction;
 
 public class Flamingo extends Application {
-	
-	private Stage primaryStage;	
+
+	private Stage primaryStage;
 	private GameHub gHub = null;
 	private BorderPane GameBorderPane = null;
 	private BorderPane BlackJackBorderPane = null;
@@ -28,7 +30,7 @@ public class Flamingo extends Application {
 	private BlackJackController BJC;
 	private GameClient gClient = null;
 	private Player appPlayer;
-	
+
 	public static void main(String[] args) {
 		launch(args);
 
@@ -40,16 +42,49 @@ public class Flamingo extends Application {
 		showClientServer(primaryStage);
 
 	}
+
 	public void setPlayer(Player player) {
 		this.appPlayer = player;
 	}
-	
-	public void showPoker(boolean bStartHub, String strComputerName, int iPort, String strPlayerName) {
+
+	public Player getAppPlayer() {
+		return appPlayer;
+	}
+
+	public void ShowBlackJack(boolean bStartHub, String strComputerName, int iPort, String strPlayerName) {
+		SetHubClient(bStartHub, strComputerName, iPort, strPlayerName);
+
+		try {
+
+			FXMLLoader loader = new FXMLLoader();
+			loader = new FXMLLoader(getClass().getResource("/game/app/view/BlackJack.fxml"));
+			BlackJackBorderPane = (BorderPane) loader.load();
+			Scene scene = new Scene(BlackJackBorderPane);
+			primaryStage.setScene(scene);
+			BJC = loader.getController();
+
+			BJC.setMainApp(this);
+			primaryStage.show();
+
+			/*
+			Action act = new Action();
+			act.setAction(eAction.TableState);
+			act.setPlayer(this.getAppPlayer());
+			this.SendMessageToClient(act);
+			*/
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void SetHubClient(boolean bStartHub, String strComputerName, int iPort, String strPlayerName) {
 
 		if (bStartHub) {
 			try {
 				gHub = new GameHub(iPort);
- 
+
 			} catch (Exception e) {
 				System.out.println("Error: Can't listen on port " + iPort);
 				return;
@@ -62,29 +97,20 @@ public class Flamingo extends Application {
 		}
 
 		setPlayer(new Player(strPlayerName, gClient.getID()));
-		
-		HandleRoot();
 
-		ShowBlackJack();		
-		
-/*		if (!bStartHub) {
-		
-			Action act = new Action();
-			act.setAction(eAction.TableState);
-			act.setPlayer(this.getPlayer());
-			this.messageSend(act);
-		}*/
+		// HandleRoot();
+
+		// ShowBlackJack();
+
 	}
-	
-	
-	
+
 	public void showClientServer(Stage primaryStage) {
 		try {
 			// Load person overview.
 			FXMLLoader loader = new FXMLLoader();
-			
-			loader =  new FXMLLoader(getClass().getResource("/game/app/view/ClientServerStart.fxml"));
-			 
+
+			loader = new FXMLLoader(getClass().getResource("/game/app/view/ClientServerStart.fxml"));
+
 			BorderPane ClientServerOverview = (BorderPane) loader.load();
 
 			Scene scene = new Scene(ClientServerOverview);
@@ -95,18 +121,18 @@ public class Flamingo extends Application {
 			ClientServerStartController controller = loader.getController();
 			controller.setMainApp(this);
 			primaryStage.show();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void HandleRoot() {
-		//Parent root;
+		// Parent root;
 		try {
-						
-			FXMLLoader loader = new FXMLLoader();			
-			loader =  new FXMLLoader(getClass().getResource("/game/app/view/GameBorder.fxml"));					
+
+			FXMLLoader loader = new FXMLLoader();
+			loader = new FXMLLoader(getClass().getResource("/game/app/view/GameBorder.fxml"));
 			GameBorderPane = (BorderPane) loader.load();
 			Scene scene = new Scene(GameBorderPane);
 			primaryStage.setScene(scene);
@@ -119,26 +145,11 @@ public class Flamingo extends Application {
 			e.printStackTrace();
 		}
 	}
-	
-	public void ShowBlackJack()
-	{		 
-		try {
-						
-			FXMLLoader loader = new FXMLLoader();			
-			loader =  new FXMLLoader(getClass().getResource("/game/app/view/BlackJack.fxml"));					
-			BlackJackBorderPane = (BorderPane) loader.load();
-			Scene scene = new Scene(BlackJackBorderPane);
-			primaryStage.setScene(scene);
-			BJC = loader.getController();
-			BJC.setMainApp(this);
-			primaryStage.show();
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void SendMessageToClient(Object Message) {
+		gClient.messageSend(Message);
 	}
-	
+
 	private class GameClient extends Client {
 
 		public GameClient(String hubHostName, int hubPort) throws IOException {
@@ -148,54 +159,46 @@ public class Flamingo extends Application {
 		/*
 		 * messageSend - One single place to send messages
 		 */
-		protected void messageSend(Object message)
-		{
-			//System.out.println("Sending message from MainApp.Client");
+		protected void messageSend(Object message) {
+			// System.out.println("Sending message from MainApp.Client");
 			resetOutput();
 			super.send(message);
 		}
-		
+
 		/*
 		 * messageReceived will get an Object message... it's up to you to determine
 		 * what should happen to that the message.
 		 * 
-		 * If it's a Table, handle Table - level action
-		 * If it's a GamePlay, handle GamePlay - level action
+		 * If it's a Table, handle Table - level action If it's a GamePlay, handle
+		 * GamePlay - level action
 		 */
 		@Override
 		protected void messageReceived(final Object message) {
-			Platform.runLater(() -> {		
-				//System.out.println("Message Received.  The message: " + message);
-				
-				if (message instanceof String)
-				{				
+			Platform.runLater(() -> {
+				// System.out.println("Message Received. The message: " + message);
+
+				if (message instanceof String) {
 					System.out.println("Message Received " + message);
+				} else if (message instanceof Table) {
+					BJC.HandleTableState((Table) message);
+				} else if (message instanceof GamePlay) {
+
 				}
-				else if (message instanceof Table)
-				{
-					
-				}
-				else if (message instanceof GamePlay)
-				{
-					
-				}
-				
+
 			});
 		}
-		
-		
+
 		@Override
 		/*
 		 * serverShutdown - Call the hard exit.
 		 */
-	    protected void serverShutdown(String message) {
-	    	
-			Platform.runLater(() -> {		
+		protected void serverShutdown(String message) {
+
+			Platform.runLater(() -> {
 				Platform.exit();
-		        System.exit(0);
+				System.exit(0);
 			});
-	    }
-	
+		}
 
 	}
 
